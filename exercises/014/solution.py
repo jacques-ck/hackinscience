ee95cf1bfa83a0c7ecbdebbcf19c735be17f0f47
Jacques <jacques.chartier-kastler@cri-paris.org>
2016-12-16 14:38:19 +0100

    hello world

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python2.7
 import sys
-if (len(sys.argv))==1:
+if (len(sys.argv)) == 1:
     print("usage: python3 solution.py PARAM")
 else:
-    a=sys.argv[1]
+    a = sys.argv[1]
     print(a)
-
